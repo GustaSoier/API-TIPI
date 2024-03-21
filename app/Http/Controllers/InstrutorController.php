4c8059f833b0c956b84ee40d12ac28ba9b67dc4e
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2024-03-21 14:45:08 -0300

    Criando uma API com laravel

---
+++ app/Http/Controllers/InstrutorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Instrutor;
+use Illuminate\Http\Request;
+
+class InstrutorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Instrutor  $instrutor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Instrutor $instrutor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Instrutor  $instrutor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Instrutor $instrutor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Instrutor  $instrutor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Instrutor $instrutor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Instrutor  $instrutor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Instrutor $instrutor)
+    {
+        //
+    }
+}
